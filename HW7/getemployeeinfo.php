75f44da22b045a359287d4dae9a80df89996ee81
Tyler Brooks <brooksrelyt@gmail.com>
2018-11-07 15:53:28 -0500

    finished hw7

--- HW7/getemployeeinfo.php
+++ HW7/getemployeeinfo.php
@@ -1,10 +1,10 @@
 <table width="639" height="66" border = "0" cellpadding = "0" cellspacing = "10">
    <tr>
+    <td width="" bgcolor = "#ffffaa">First Name </td>
      <td width="" bgcolor = "#ffffaa">Last Name </td>
      <td width="" bgcolor = "#ffffbb">Title</td>
      <td width="" bgcolor = "#ffffbb">Department</td>
      <td width="" bgcolor = "#ffffbb">Department Location</td>
-     <td width="" bgcolor = "#ffffbb">Department Phone </td>
      
    </tr>
  
@@ -20,9 +20,10 @@
           }
          $Employee_lname = $_REQUEST['Employee_lname'];
           // first query - get the data, ownerID and FoodID from the Pets table
-          if ( !( $result = $database->query ( "SELECT Employee_lname,Employee_title,Department_id FROM Employee WHERE Employee_lname = '$Employee_lname'") ) )
+          if ( !( $result = $database->query ( "SELECT Employee_fname,Employee_lname,Employee_title,Department_id FROM Employee WHERE Employee_lname = '$Employee_lname'") ) )
               print ("Warning!   could not execute query <br />" );
          $row = $result->fetch_assoc();
+          $Employee_fname = $row["Employee_fname"];
           $Employee_lname = $row["Employee_lname"];
           $Employee_title = $row["Employee_title"];
           $Department_id = $row["Department_id"];
@@ -33,13 +34,13 @@
          $row = $result->fetch_assoc();
           $Department_name = $row["Department_name"];
           $Department_location = $row["Department_location"];
-          $Department_phone_ext = $row["Department_phone_ext"];
  
         // Now just print each form field’s value
-          print( "<td>$Employee_lname</td>
+          print( "<td>$Employee_fname</td>
+            <td>$Employee_lname</td>
+                    <td>$Employee_title</td>
                      <td>$Department_name</td>
-                     <td>$Department_location</td>
-                     <td>$Department_phone_ext</td>" );
+                     <td>$Department_location</td>" );
                     
            // and wrap up the table
            print("</tr></table><br><br>")
